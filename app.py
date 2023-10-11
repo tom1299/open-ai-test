8bf95d5a6013dfbed12ce9fbf367ff0c386fb5b0
tom1299 <tom1299@jeeatwork.com>
2023-10-11 07:26:41 +0200

    Add temprature to create more diverse responses

--- app.py
+++ app.py
@@ -6,24 +6,27 @@ import logging
 # Caution: Might print the openai key to the console
 import http.client as http_client
 
-http_client.HTTPConnection.debuglevel = 1
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+if os.getenv("DEBUG"):
+    http_client.HTTPConnection.debuglevel = 1
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
 
 openai.debug = True
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-    model="gpt-3.5-turbo-instruct",
-    prompt="Say this is a test!",
-    temperature=1,
-    max_tokens=256
-)
+# Let chatgpt be creative by adding temperature to the completion of a very famous quote
+for temperature in [1, 3, 5, 7, 9]:
+    response = openai.Completion.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt="To be or not to be",
+        temperature=temperature/10,
+        max_tokens=256
+    )
 
-for choice in response["choices"]:
-    print(choice["text"])
-    print(choice["finish_reason"])
-    print(choice["index"])
+    for choice in response["choices"]:
+        print(f"Temperature: {temperature/10}")
+        print(f"Text: {choice['text']}")
+
